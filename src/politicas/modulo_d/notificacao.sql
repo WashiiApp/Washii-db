10630722ec751b53e3a6bbed834c7603f82ea10d
Carlos Henrique <carlosouza.dev@gmail.com>
2026-08-25 21:40:56 -0300

    corrige script

--- src/politicas/modulo_d/notificacao.sql
+++ src/politicas/modulo_d/notificacao.sql
@@ -1,18 +1,24 @@
--- SELECT: apenas dono do veículo e o lava jato veem/recebem
-CREATE POLICY "notificacao_select"
+CREATE POLICY "notificacao_select_scope"
 ON notificacao
 FOR SELECT
 TO authenticated
 USING (
   EXISTS (
     SELECT 1 FROM agendamento a
-    LEFT JOIN veiculo v ON v.id = a.id_veiculo
+    JOIN veiculo v ON v.id = a.id_veiculo
     WHERE a.id = notificacao.id_agendamento
-      AND (a.id_lava_jato = auth.uid() OR v.id_cliente = auth.uid())
+      AND v.id_cliente = auth.uid()
+  )
+  OR EXISTS (
+    SELECT 1 FROM agendamento_servico ags
+    JOIN servico s ON s.id = ags.id_servico
+    WHERE ags.id_agendamento = notificacao.id_agendamento
+      AND s.id_lavajato = auth.uid()
   )
 );
 
--- INSERT: sistema/usuários podem criar notificação para o agendamento
+-- INSERT
+
 CREATE POLICY "notificacao_insert"
 ON notificacao
 FOR INSERT
@@ -20,30 +26,47 @@ TO authenticated
 WITH CHECK (
   EXISTS (
     SELECT 1 FROM agendamento a
-    LEFT JOIN veiculo v ON v.id = a.id_veiculo
+    JOIN veiculo v ON v.id = a.id_veiculo
     WHERE a.id = notificacao.id_agendamento
-      AND (a.id_lava_jato = auth.uid() OR v.id_cliente = auth.uid())
+      AND v.id_cliente = auth.uid()
+  )
+  OR EXISTS (
+    SELECT 1 FROM agendamento_servico ags
+    JOIN servico s ON s.id = ags.id_servico
+    WHERE ags.id_agendamento = notificacao.id_agendamento
+      AND s.id_lavajato = auth.uid()
   )
 );
 
--- UPDATE: pode alterar o statu, marcar como "lida" apenas quem está envolvido no agendamento
-CREATE POLICY "notificacao_update_scope"
+CREATE POLICY "notificacao_update"
 ON notificacao
 FOR UPDATE
 TO authenticated
 USING (
   EXISTS (
     SELECT 1 FROM agendamento a
-    LEFT JOIN veiculo v ON v.id = a.id_veiculo
+    JOIN veiculo v ON v.id = a.id_veiculo
     WHERE a.id = notificacao.id_agendamento
-      AND (a.id_lava_jato = auth.uid() OR v.id_cliente = auth.uid())
+      AND v.id_cliente = auth.uid()
+  )
+  OR EXISTS (
+    SELECT 1 FROM agendamento_servico ags
+    JOIN servico s ON s.id = ags.id_servico
+    WHERE ags.id_agendamento = notificacao.id_agendamento
+      AND s.id_lavajato = auth.uid()
   )
 )
 WITH CHECK (
   EXISTS (
     SELECT 1 FROM agendamento a
-    LEFT JOIN veiculo v ON v.id = a.id_veiculo
+    JOIN veiculo v ON v.id = a.id_veiculo
     WHERE a.id = notificacao.id_agendamento
-      AND (a.id_lava_jato = auth.uid() OR v.id_cliente = auth.uid())
+      AND v.id_cliente = auth.uid()
+  )
+  OR EXISTS (
+    SELECT 1 FROM agendamento_servico ags
+    JOIN servico s ON s.id = ags.id_servico
+    WHERE ags.id_agendamento = notificacao.id_agendamento
+      AND s.id_lavajato = auth.uid()
   )
 );
